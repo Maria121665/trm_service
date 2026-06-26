89a94a56d43fcaf45724d013ac42cdbc484dae51
Maria121665 <info@carhidrasas.com>
2026-06-26 17:25:25 -0400

    Update currency endpoints to use new API

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 @app.route("/usd")
 def usd():
-    data = requests.get("https://s3.amazonaws.com/dolartoday/data.json").json()
-    return jsonify({"usd": data["USD"]["promedio"]})
+    data = requests.get("https://api.exchangerate.host/latest?base=USD&symbols=VES").json()
+    return jsonify({"usd": data["rates"]["VES"]})
 
 @app.route("/eur")
 def eur():
-    data = requests.get("https://s3.amazonaws.com/dolartoday/data.json").json()
-    return jsonify({"eur": data["EUR"]["promedio"]})
+    data = requests.get("https://api.exchangerate.host/latest?base=EUR&symbols=VES").json()
+    return jsonify({"eur": data["rates"]["VES"]})
+
