7617e8b822d7897d414146be21fbd734b753794d
Maria121665 <info@carhidrasas.com>
2026-06-26 17:27:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 @app.route("/usd")
 def usd():
-    data = requests.get("https://api.exchangerate.host/latest?base=USD&symbols=VES").json()
+    data = requests.get("https://open.er-api.com/v6/latest/USD").json()
     return jsonify({"usd": data["rates"]["VES"]})
 
 @app.route("/eur")
 def eur():
-    data = requests.get("https://api.exchangerate.host/latest?base=EUR&symbols=VES").json()
+    data = requests.get("https://open.er-api.com/v6/latest/EUR").json()
     return jsonify({"eur": data["rates"]["VES"]})
+
 
